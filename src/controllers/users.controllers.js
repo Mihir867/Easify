21c83b1e6d8f3897d3461ec4f878dbaca53fd539
Mihir Thakur <Mihirkantthakur867@gmail.com>
2024-01-08 13:31:18 +0530

    Changes done

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -3,11 +3,12 @@ import ProductModel from "../models/users.model.js";
 import { products } from "../models/users.model.js";
 export default class UserControllers{
 
-    getProducts(req, res){
-        let products = ProductModel.get();
-        console.log(products);
-        res.render("jobs", {products});
-    };
+    getProducts(req, res) {
+        const productsData = ProductModel.get();
+        const jobDetails = productsData.length > 0 ? productsData[0] : {}; // Assuming there is at least one job
+        res.render("jobs", { products: productsData, jobDetails });
+    }
+    
 
     addProducts(req, res){
         ProductModel.add(req.body);
@@ -27,7 +28,9 @@ export default class UserControllers{
     }
 
     getDetails(req,res){
-        return res.render('details');
+        const jobId = parseInt(req.params.id);
+    const product = ProductModel.getbyid(jobId);
+    return res.render('details', { product });
     }
     getPostjobs(req,res){
         return res.render('post');
